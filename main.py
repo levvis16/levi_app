bf473fa6748ab2500c4bd13f9cfe300c105a5420
levvis16 <zoomcross2@gmail.com>
2026-05-14 17:06:47 +0300

    beta-version app 2.0

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from database.database import engine
 from database.models import Base
 from routers import user, group, dialogs
+from fastapi.staticfiles import StaticFiles
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,6 +13,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(user.router)
 app.include_router(group.router)
 app.include_router(dialogs.router)
