d6613c8004e3dc448cd0401750fe99c704c39aff
levvis16 <zoomcross2@gmail.com>
2026-05-22 11:16:23 +0300

    add telegram bot + rabbitmq queue

---
+++ telegram_app/main.py
@@ -0,0 +1,43 @@
+import asyncio                           
+import logging
+import os               
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher, types     
+from aiogram.filters import Command        
+from faststream.rabbit import RabbitBroker
+
+load_dotenv()
+dp = Dispatcher()                        
+broker = RabbitBroker()
+
+ADMIN_ID = os.getenv("ADMIN_ID")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@broker.subscriber("help")
+async def handle_help(data: str):
+    await bot.send_message(
+        chat_id=int(ADMIN_ID),
+        text= data
+    )
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer(f"привет lev")
+
+async def main() -> None:
+    await broker.start()
+    logging.info('брокер стартовал')
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await broker.close()
+        logging.info('брокер остановлен')
+
+if __name__ == "__main__":
+    logging.info("запуск приложения")
+    asyncio.run(main())
